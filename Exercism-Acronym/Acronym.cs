a44a1c1ea30bf0a742a41d00946f5aafe7ac7a80
Meryame <meryame.zerhouni@gmail.com>
2022-05-24 09:57:50 +0200

    first commit

---
+++ Exercism-Acronym/Acronym.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism_Acronym
+{
+    public class Acronym
+    {
+        public static string Abbreviate(string phrase)
+        {
+            var result = "";
+            char[] separators = { ' ', '-', '_', ',' };
+            var words = phrase.Split(separators , StringSplitOptions.RemoveEmptyEntries);
+            foreach (var word in words)
+            {
+                result += word.ToUpper()[0];
+            }
+            return result;
+        }
+    }
+}
---
+++ Exercism-Acronym/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using Exercism_Acronym;
+
+var acronym = Acronym.Abbreviate("Back End");
+Console.WriteLine(acronym);
+Console.ReadKey();
